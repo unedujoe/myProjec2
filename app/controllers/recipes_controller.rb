7d1ad9627c2812f1857839f6cb9d31c963de0b66
Joseph <unedujoe@gmail.com>
2016-06-09 17:29:58 +0000

    Add admin functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   before_action  :require_user, except: [:show, :index, :like]
    before_action   :require_user_like,  only: [:like]
   before_action   :require_same_user,  only: [:edit, :update]
+  before_action  :admin_user, only: :destroy
   
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -55,6 +56,11 @@ class RecipesController < ApplicationController
      
   end
   
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success]= "Recipe Deleted"
+    redirect_to recipes_path
+  end
   
   
   private
@@ -69,7 +75,7 @@ class RecipesController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @recipe.chef
+    if current_user != @recipe.chef and !current_user.admin?
       flash[:danger] ="You can only edit your own recipes"
       redirect_to recipes_path
     end
@@ -80,6 +86,11 @@ class RecipesController < ApplicationController
       flash[:danger] ="You must be loged in to perform  that action"
       redirect_to :back
     end
+  end
+  
+  def admin_user
+    redirect_to recipes_path unless current_user.admin?
+    
   end
   
 end
